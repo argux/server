5c1b7b0577a22ffd83e3499bb82f1e4c30064191
Stephan Arts <stephan@xfce.org>
2016-07-10 22:18:37 +0200

    Allow removing bookmarks from a users bookmark page

--- argux_server/static/js/source/profile.js
+++ argux_server/static/js/source/profile.js
@@ -1,6 +1,22 @@
 var bookmarks;
 
 $(function() {
+
+    /*
+     * Create callback hooks for removing users.
+     */
+    function create_remove_callbacks(obj) {
+        $('.bookmark-remove').click(function() {
+            var bookmark_id = $(this).parents('tr').data('bookmark');
+
+            $('#remove-bookmark').val(bookmark_id);
+            $('#remove-bookmark-message').text(
+                'Do you want to remove the bookmark "' +
+                bookmark_id + '"');
+            $('#remove-bookmark-modal').modal('show');
+        });
+    }
+
     function get_bookmarks_success_callback(json) {
         if (VIEW_SORT === "desc") {
             bookmarks = json.bookmarks.sort(function(a, b) {return a.title < b.title});
@@ -15,23 +31,31 @@ $(function() {
         list = $('#bookmark-list-body');
         list.empty();
 
-        $.each(bookmarks, function(i, value) {
+        if(bookmarks.length > 0) {
+            $.each(bookmarks, function(i, value) {
+                list.append(
+                    '<tr data-bookmark="'+value.id+'" class="success">' +
+                    '<td>'+
+                    '<a class="host" href="'+value.url+'">' +
+                    '<span class="glyphicon glyphicon-bookmark"></span> '+
+                    value.title +
+                    '</a>' +
+                    '<div class="pull-right">' +
+                    '<a href="#" class="bookmark-remove">'+
+                    '<span class="glyphicon glyphicon-remove"></span>'+
+                    '</a>'+
+                    '</div>' +
+                    '</td>' +
+                    '</tr>'
+                );
+            });
+        } else {
             list.append(
-                '<tr data-bookmark="'+value.id+'">' +
-                '<td>'+
-                '<a class="host" href="'+value.url+'">' +
-                '<span class="glyphicon glyphicon-bookmark"></span> '+
-                value.title +
-                '</a>' +
-                '<div class="pull-right">' +
-                '<a href="%">'+
-                '<span class="glyphicon glyphicon-remove"></span>'+
-                '</a>'+
-                '</div>' +
-                '</td>' +
-                '</tr>'
+                '<tr><td><em>Empty</em></tr>'
             );
-        });
+        }
+
+        create_remove_callbacks();
     }
 
     if (ACTION === 'bookmarks') {
@@ -50,8 +74,20 @@ $(function() {
                 $('.bookmarks-sort-asc').addClass('active');
                 $('.bookmarks-sort-desc').removeClass('active');
             }
+
             update_view();
         });
     }
+
+    $('#remove-bookmark-form').submit(function(event) {
+        user.delete_bookmark({
+            'bookmark': $('#remove-bookmark').val()
+        });
+        $('#remove-bookmark-modal').modal('hide');
+
+        user.get_bookmarks({
+            success : get_bookmarks_success_callback
+        });
+    });
 });
 
